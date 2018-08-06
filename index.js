31248ca4d85b8e0be4eeb090aa197ad4c81265d9
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-08-06 15:40:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,15 +6,24 @@ function updateDriverWithKeyAndValue(driver, key, value) {
   newDriver = {...driver};
   newDriver[key] = value;
   return newDriver;
-  
+
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   driver[key] = value;
   return driver;
+
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  newDriver = {...driver};
+  delete newDriver[key];
+  return newDriver;
   
 }
 
 function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key];
+  return driver;
   
 }
